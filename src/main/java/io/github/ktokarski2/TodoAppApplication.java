84cd48eadf1a16689a7d95e3d127c3cfc5242091
KTokarski2 <krzysztof.tokarski2@icloud.com>
2023-08-06 14:43:30 +0200

    finished editing configuration files

--- src/main/java/io/github/ktokarski2/TodoAppApplication.java
+++ src/main/java/io/github/ktokarski2/TodoAppApplication.java
@@ -3,9 +3,11 @@ package io.github.ktokarski2;
 import jakarta.validation.Validator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
 
 @SpringBootApplication
 public class TodoAppApplication {
